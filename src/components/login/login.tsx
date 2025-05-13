494296c329c0ad7912ae1d896f7fba6962c541cc
Vladislav KS <spike.kvs@gmail.com>
2025-05-13 14:53:31 +0300

    fix auth logic

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,12 +1,20 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '..//../firebase';
+import {useAppDispatch} from '../../hooks';
+import {checkAuthAction} from '../../store/user-slice/user-api-actions.ts';
+import {useNavigate} from 'react-router-dom';
+import {AppRoute} from '../../utils/const.ts';
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   const login = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-      .then((result) => {
-        console.log('Вход через Google успешен:', result.user);
+      .then(() => {
+        dispatch(checkAuthAction());
+        navigate(AppRoute.ROOT);
       })
       .catch((error) => {
         console.error('Ошибка входа через Google:', error.message);
@@ -14,7 +22,7 @@ const Login = () => {
   }
 
   return (
-    <button onClick={login} className='login__google'>Google</button>
+      <button onClick={login} className='login__google'>Google</button>
   );
 }
 
